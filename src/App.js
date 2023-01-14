f0a875a86712a8cdc40be3555c3ae828e2ff747b
Mohammad Ismail Alam <ismail.alam@konasl.com>
2023-01-14 22:10:11 +0600

    one img added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import "./App.css";
 
+
 function App() {
   return (
     <>
-    <div className="container">
-    <div className="row">
-        <div className="col-6">
-          <div className="App">
-            <h1> Create Your Treat</h1>
+      <div className="container">
+        <div className="App">
+          <h1> Create Your Treat</h1>
+          
+          <div class="menu_selection">
+            <h3>Menu Create Options</h3>
+            <div>
+              <span class="of_notice">Choose Menu creation option</span>
+            </div>
+            <ul class="check_box">
+              <li>
+                <input
+                  name="menuTypeList"
+                  type="checkbox"
+                  class="MenuInput"
+                  value="Menu"
+                />
+                <label class="checkbox " for="Menu">
+                  Menu
+                </label>
+              </li>
+              <li>
+                <input
+                  name="menuTypeList"
+                  type="checkbox"
+                  class="manualMenuInput"
+                  value="manualMenu"
+                />
+                <label class="checkbox" for="manualMenu">
+                  Manual Menu
+                </label>
 
-            <div className="selection">
+              </li>
+            </ul>
+          </div>
+
+          <div className="selection">
             <div className="img">
               <div>
                 <img src="addImg.png" alt=""></img>
@@ -28,34 +59,29 @@ function App() {
             </div>
 
             <div class="time">
-            <h3> Set Time</h3>
+              <h3> Set Time</h3>
               <input type="time" placeholder="Time Limite"></input>
             </div>
           </div>
-          </div>
         </div>
-      </div>
-      <div className="col-6">
-            <div className="App">
-              <form>
-                <div className="input-group">
-                  <label htmlFor="name">Name </label>
-                  <input type="text" id="name" />
-                </div>
-                <div className="input-group">
-                  <label htmlFor="price">Item Price</label>
-                  <input type="cost" id="price" />
-                </div>
-                <button type="submit" className="add-btn">
-                  + Add new items
-                </button>
-              </form>
+
+        <div className="App">
+          <form>
+            <div className="input-group">
+              <label htmlFor="name">Name </label>
+              <input type="text" id="name" />
             </div>
-          </div>
-          <div className="details">
-            
-          </div>
-    </div>
+            <div className="input-group">
+              <label htmlFor="price">Item Price</label>
+              <input type="cost" id="price" />
+            </div>
+            <button type="submit" className="add-btn">
+              + Add new items
+            </button>
+          </form>
+        </div>
+        <div className="details"></div>
+      </div>
     </>
   );
 }
