fa8412c1023bb21a2c61e924dca840d64d2fc174
mohammadismail <ismailalam901@gmail.com>
2023-01-16 16:40:29 +0600

    ManualMenu.js

---
+++ src/Menu/ManualMenu.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { useState } from "react";
+
+export default function ManualMenu( { setName } ) {
+
+  const [isShow, setIsShow] = useState(false);
+
+  const handleShow = (e) => {
+    setIsShow(e.target.checked)
+  };
+
+  return (
+    <div className="menu-selection">
+      <ul>
+        <li>
+          <input
+            name="menuTypeList"
+            type="checkbox"
+            class="manualMenuInput"
+            value="manualMenu"
+            id="manualMenu"
+            onChange={handleShow}
+            
+          />
+          <label class="checkbox" for="manualMenu" >
+            Manual Menu
+          </label>
+        </li>
+
+        {isShow && (
+          <>
+            <div className="manualMenu">
+              <form>
+                <div className="input-group">
+                  <label htmlFor="name">Name </label>
+                  <input type="text" id="name" onChange={setName} />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="price">Item Price</label>
+                  <input type="cost" id="price" />
+                </div>
+                <div className="input-group">
+                <button type="submit" className="add-btn">
+                  + Add
+                </button>
+                </div>
+               
+              </form>
+            </div>
+
+          </>
+        )}
+      </ul>
+    </div>
+  );
+}
