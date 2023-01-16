b8148972962f099072a56a2513f91b7edaa72b83
mohammadismail <ismailalam901@gmail.com>
2023-01-16 16:39:23 +0600

    MenuCreate.js

---
+++ src/Menu/menuCreate.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useState } from "react";
+
+export default function MenuCreate() {
+  const [isShow, setIsShow] = useState(false);
+  const handleShow = (e) => {
+    setIsShow(e.target.checked)
+  };
+
+
+  return (
+    <div class="menu_selection">
+      <h3>Menu Create Options</h3>
+        <span class="of_notice">Choose Menu creation option</span>
+      <ul class="check_box">
+        <li>
+          <input
+            name="menuTypeList"
+            type="checkbox"
+            class="MenuInput"
+            value="Menu"
+            id="Menu"
+            onChange={handleShow}
+          />
+          <label class="checkbox " for="Menu">
+            Menu
+          </label>
+        </li>
+
+        {
+          isShow &&
+          <div className="img">
+              <input
+                type="file"
+                accept="image/jpeg, image/png, image/jpg"
+              ></input>
+          </div>
+        }
+      </ul>
+    </div>
+  );
+}
