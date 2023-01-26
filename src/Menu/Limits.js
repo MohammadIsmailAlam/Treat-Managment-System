9b50908f5d22175a25b74d28e4592b22924b4220
mohammadismail <ismailalam901@gmail.com>
2023-01-26 16:52:28 +0600

    Css added

--- src/Menu/Limits.js
+++ src/Menu/Limits.js
@@ -5,17 +5,17 @@ export default function Limits( {setbudget, settime} ) {
 
   return (
     <div className='limits'>
-      <div className='budget'>
-        <label htmlFor="budget"> Budget Limit</label> 
-        <input type="number" id='budget' className='budget' 
+      <div className='budget mb-3'>
+        <label htmlFor="budge floatingInputt"> Budget Limit</label> 
+        <input type="number" id='budget' className='form-control' 
         onChange={(event) => { 
           event.preventDefault();
           setbudget(event.target.value)}}/>
       </div>
 
-      <div className='time'>
-        <label htmlFor="time"> Time Limit</label>
-        <input type="time" id='time' className='time' 
+      <div className='time mb-3'>
+        <label htmlFor="time floatingInputt"> Time Limit</label>
+        <input type="time" id='time' className='form-control' 
         onChange={(event) => {
           event.preventDefault();
           settime(event.target.value)}}/>
