c0b731be1f0f54f7afc999f22fbd211de16dbb65
mohammadismail <ismailalam901@gmail.com>
2023-02-03 17:39:12 +0600

    Add css for img, so that fullscreen so full img

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -44,24 +44,27 @@ const Details = () => {
                 </tbody>
             </table>
         }
-        <img className={fullScreen && "fullScreen"}
-            src={location.state?.img}
-            alt=""
-            onClick={toggleFullScreen}
-            style={{
-                cursor: "pointer",
-                // maxWidth: fullScreen?  "100vw !important" : "250px",
-                width: fullScreen ? "100vh" : "auto",
-                height: fullScreen ? "100vw" : "auto",
-                position: fullScreen ? "fixed" : "static",
-                top: fullScreen ? 0 : "auto",
-                left: fullScreen ? 0 : "auto",
-                right: fullScreen ? 0 : "auto",
-                bottom: fullScreen ? 0 : "auto",
-                backgroundColor: "rgba(0,0,0,0.5)",
-                zIndex: fullScreen ? 1000 : "auto",
-            }}
-        />
+       <img className={fullScreen && "fullScreen"}
+  src={location.state?.img}
+  alt=""
+  onClick={toggleFullScreen}
+  style={{
+    cursor: "pointer",
+    maxWidth: "100%",
+    maxHeight: "100%",
+    width: fullScreen ? "auto" : "250px",
+    height: "auto",
+    position: fullScreen ? "fixed" : "static",
+    top: fullScreen ? 0 : "auto",
+    left: fullScreen ? 0 : "auto",
+    right: fullScreen ? 0 : "auto",
+    bottom: fullScreen ? 0 : "auto",
+    backgroundColor: "rgba(0,0,0,0.5)",
+    zIndex: fullScreen ? 1000 : "auto",
+    overflow: fullScreen ? "auto" : "hidden"
+  }}
+/>
+
         <p>Budget Limit: {location.state?.budget}</p>
         <p>Time Limit: {location.state?.time}</p>
     </div>
