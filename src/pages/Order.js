e672b4061cb93ed660eb3a8e67a628333b97fba8
mohammadismail <ismailalam901@gmail.com>
2023-02-16 09:59:37 +0600

    get id for a specific list

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom';
 
 export default function Order() {
     const [values, setValues] = useState([]);
     const [selectedItem, setSelectedItem] = useState([]);
-    useEffect(() => {
-        fetch("http://localhost:3001/treats")
-            .then((response) => response.json())
-            .then((data) => {
-                setValues(data[0]);
-                console.log(data[0]);
-            });
-    }, []);
+    const [remainingTime, setRemainingTime] = useState(0);
+
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const tradeId = searchParams.get('tradeId');
+
+    console.log(tradeId);
+    // useEffect(() => {
+    //     alert(tradeId)
+    // }, [])
 
     const handleChecked = (e) => {
         const id = e.target.value;
@@ -23,13 +26,39 @@ export default function Order() {
         });
         console.log("selected");
     };
-    
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/treats/${tradeId}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setValues(data);
+                setRemainingTime(data.timeLimit * 60 * 1000);
+                console.log(data);
+            });
+    }, []);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setRemainingTime(remainingTime - 1000);
+        }, 1000);
+            clearInterval(intervalId);
+    }, [remainingTime]);
+
+
+    const formatTime = (time) => {
+        const hours = Math.floor( (time/(1000*60*60)) % 24 );
+        const minutes = Math.floor(time / 60000);
+        const seconds = Math.floor((time % 60000) / 1000);
+
+        return `${hours}:${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
         if (selectedItem.length > 0 && values.manualMenuList) {
             const selectedItemsData = values.manualMenuList.filter(item => selectedItem.includes(item._id));
             if (selectedItemsData.length > 0) {
-                alert(`Selected items: ${selectedItemsData.map(item => `${item.name}, ${item._id}`)}`);
+                console.log(`Selected items: ${selectedItemsData.map(item => `${item.name} ${item._id}`)}`);
             }
         }
     };
@@ -38,7 +67,7 @@ export default function Order() {
         <div className="container">
             <div className="row">
                 <div className="ordersSeelection">
-                    <header style={{textAlign: "center", marginTop: "10px"}}>
+                    <header style={{ textAlign: "center", marginTop: "10px" }}>
                         Order Form Here....
                     </header>
                     <div className="title" style={{
@@ -47,9 +76,14 @@ export default function Order() {
                         backgroundColor: "lightblue",
                         fontSize: "24px"
                     }}>
-                        <p style={{fontFamily: "monospace"}}>
-                            Budget: {values.budgetLimitPerPerson} Time: {values.timeLimit}</p>
+                        <p style={{ fontFamily: "monospace" }}>
+                            Budget: {values.budgetLimitPerPerson}</p>
                     </div>
+
+                    <div style={{ textAlign: 'center' }}>
+                        <p>Time Limit: {formatTime(remainingTime)}</p>
+                    </div>
+
                     <form className='order' style={{ display: "block" }} onSubmit={handleSubmit}>
                         {values?.manualMenuList?.map((_data, index) => (
                             <li key={index}
