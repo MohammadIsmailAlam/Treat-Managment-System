a24d47b51f2f0ef877ad8975fc8c5f4afd041e1e
mohammadismail <ismailalam901@gmail.com>
2023-02-21 17:11:51 +0600

    addedorderedlist component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import Details from "./pages/Details";
 import Landing from "./pages/Landing";
 import Order from "./pages/Order";
+import OrderedList from "./Menu/OrderedList";
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route exact path="/details" element={<Details/>}/>
         <Route exact path="/order" element= {<Order/>}/>
         <Route exact path="/order/:treatId" element= {<Order/>}/>
+        <Route exact path="/orderedList" element= {<OrderedList/>}/>
         </Routes>
       </Router>
     </>
