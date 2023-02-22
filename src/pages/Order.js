625db0b67298777ad3ab2d8015f4ac23d78ba815
mohammadismail <ismailalam901@gmail.com>
2023-02-22 18:22:51 +0600

    gets the successfull message from successfulMsg component

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-// import SuccessMsg from "../Menu/SuccessMsg";
+import SuccessMsg from "../Components/SuccessMsg";
 
 export default function Order() {
   const [values, setValues] = useState(null);
   const [selectedItems, setSelectedItems] = useState([]);
-  
-  // const [show, setShow] = useState(false)
+
+  const [showSucccess, setShowSuccess] = useState(false);
 
   const [name, setName] = useState("");
   const [nameError, setNameError] = useState(false);
@@ -27,42 +27,41 @@ export default function Order() {
       });
   }, [treatId]);
 
-const handleChecked = (e, itemName) => {
-  const checked = e.target.checked;
+  const handleChecked = (e, itemName) => {
+    const checked = e.target.checked;
 
-  setSelectedItems((prevSelectedItems) => {
-    if (checked) {
-      return [...prevSelectedItems, itemName];
-    } else {
-      return prevSelectedItems.filter((item) => item !== itemName);
+    setSelectedItems((prevSelectedItems) => {
+      if (checked) {
+        return [...prevSelectedItems, itemName];
+      } else {
+        return prevSelectedItems.filter((item) => item !== itemName);
+      }
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      setNameError(true);
+      return;
     }
-  });
-};
 
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-
-  if (!name) {
-    setNameError(true);
-    return;
-  }
-
-  const selectedItemsData = values?.manualMenuList?.filter((item) =>
-    selectedItems.includes(item.name)
-  );
-
-  if (selectedItemsData.length > 0) {
-    console.log(
-      `Selected items: ${selectedItemsData.map(
-        (item) => `${item.name} ${item.price}`
-      )}`
+    const selectedItemsData = values?.manualMenuList?.filter((item) =>
+      selectedItems.includes(item.name)
     );
-  }
 
-  console.log("the items are", selectedItems);
+    if (selectedItemsData.length > 0) {
+      console.log(
+        `Selected items: ${selectedItemsData.map(
+          (item) => `${item.name} ${item.price}`
+        )}`
+      );
+    }
 
-  // Patch/Update
+    console.log("the items are", selectedItems);
+
+    // Patch/Update
     fetch(
       `https://treat-management-system-691e2-default-rtdb.firebaseio.com/treats/${treatId}.json`,
       {
@@ -81,124 +80,132 @@ const handleSubmit = (e) => {
             }),
           ],
           budgetLimitPerPerson: values.budgetLimitPerPerson,
-          timeLimit: values.timeLimit
+          timeLimit: values.timeLimit,
         }),
       }
     )
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        // setShow(true)
       });
+
+    setShowSuccess(true);
+    console.log(showSucccess);
+
+    setSelectedItems([]);
+    setName("");
+    setNameError(false);
   };
 
   return (
     <div className="container">
-      <div className="row">
-        <div className="ordersSeelection">
-          <header style={{ textAlign: "center", marginTop: "10px" }}>
-            Order Form Here....
-          </header>
-          {values && (
-            <div
-              className="title"
-              style={{
-                textAlign: "center",
-                marginTop: "10px",
-                backgroundColor: "lightblue",
-                fontSize: "24px",
-              }}
-            >
-              <p style={{ fontFamily: "monospace" }}>
-                Budget: {values?.budgetLimitPerPerson}
-              </p>
-            </div>
-          )}
-
-          <div style={{ textAlign: "center" }}>
-            <p>Time Limit: {values?.timeLimit}</p>
-          </div>
-
-          <form
-            className="order"
-            style={{ display: "block" }}
-            onSubmit={handleSubmit}
-          >
-            {values?.manualMenuList?.map((data, index) => (
-              <li
-                key={index}
+      {showSucccess ? (
+        <SuccessMsg />
+      ) : (
+        <div className="row">
+          <div className="ordersSeelection">
+            <header style={{ textAlign: "center", marginTop: "10px" }}>
+              Order Form Here....
+            </header>
+            {values && (
+              <div
+                className="title"
                 style={{
-                  border: "1px solid grey",
-                  borderRadius: "12px",
+                  textAlign: "center",
+                  marginTop: "10px",
+                  backgroundColor: "lightblue",
+                  fontSize: "24px",
+                }}
+              >
+                <p style={{ fontFamily: "monospace" }}>
+                  Budget: {values?.budgetLimitPerPerson}
+                </p>
+              </div>
+            )}
+
+            <div style={{ textAlign: "center" }}>
+              <p>Time Limit: {values?.timeLimit}</p>
+            </div>
+
+            <form
+              className="order"
+              style={{ display: "block" }}
+              onSubmit={handleSubmit}
+            >
+              {values?.manualMenuList?.map((data, index) => (
+                <li
+                  key={index}
+                  style={{
+                    border: "1px solid grey",
+                    borderRadius: "12px",
+                    padding: "2em",
+                    margin: "2em",
+                    background: "aliceblue",
+                    position: "relative",
+                  }}
+                >
+                  <div className="checkboxes">
+                    <label>
+                      <input
+                        type="checkbox"
+                        value={data.name}
+                        onChange={(e) => handleChecked(e, data.name)}
+                      />
+                      {data.name} ---- {data.price}
+                    </label>
+                  </div>
+                </li>
+              ))}
+              <div className="form-group">
+                <label htmlFor="name">Name </label>
+                <input
+                  type="text"
+                  name="name"
+                  className="form-control"
+                  value={name}
+                  style={{ marginRight: "10px" }}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                    setNameError(false);
+                  }}
+                />
+
+                {nameError && (
+                  <div
+                    className="error"
+                    style={{ color: "red", marginTop: "10px" }}
+                  >
+                    {" "}
+                    Name Can't Be Empty
+                  </div>
+                )}
+              </div>
+              <div
+                className="input"
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
                   padding: "2em",
                   margin: "2em",
-                  background: "aliceblue",
-                  position: "relative",
                 }}
               >
-                <div className="checkboxes">
-                  <label>
-                  <input
-                      type="checkbox"
-                      value={data.name}
-                      onChange={(e) => handleChecked(e, data.name)}
-                    />
-
-                    {data.name} ---- {data.price}
-                  </label>
-                </div>
-              </li>
-            ))}
-            <div className="form-group">
-              <label htmlFor="name">Name </label>
-              <input
-                type="text"
-                name="name"
-                className="form-control"
-                value={name}
-                style={{ marginRight: "10px" }}
-                onChange={(e) => {
-                  setName(e.target.value);
-                  setNameError(false);
-                }}
-              />
-
-              {nameError && (
-                <div
-                  className="error"
-                  style={{ color: "red", marginTop: "10px" }}
+                <button
+                  type="submit"
+                  style={{
+                    border: "1px solid grey",
+                    borderRadius: "12px",
+                    background: "aliceblue",
+                    marginLeft: "10px",
+                    position: "relative",
+                  }}
                 >
-                  {" "}
-                  Name Can't Be Empty
-                </div>
-              )}
-            </div>
-            <div
-              className="input"
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                padding: "2em",
-                margin: "2em",
-              }}
-            >
-              <button
-                type="submit"
-                style={{
-                  border: "1px solid grey",
-                  borderRadius: "12px",
-                  background: "aliceblue",
-                  marginLeft: "10px",
-                  position: "relative",
-                }}
-              >
-                Submit
-              </button>
-              {/* {show && <SuccessMsg />} */}
-            </div>
-          </form>
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
