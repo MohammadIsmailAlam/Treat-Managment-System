e53df995fba5c1987b4f2d46ad3d6ad00fd447d6
mohammadismail <ismailalam901@gmail.com>
2023-02-23 11:00:46 +0600

    class to className

--- src/Components/SuccessMsg.js
+++ src/Components/SuccessMsg.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function SuccessMsg() {
   return (
-    <div class="success-message" style={{ textAlign: "center", marginTop: "20px" }}>
-      <i class="fa fa-check-circle"></i>
+    <div className="success-message" style={{ textAlign: "center", marginTop: "20px" }}>
+      <i className="fa fa-check-circle"></i>
       <h2>Success!</h2>
       <p>Your action was completed successfully.</p>
     </div>
