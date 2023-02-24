04ee1f0ca84aa03b525206c59b52e27d338ad59c
mohammadismail <ismailalam901@gmail.com>
2023-02-24 14:34:50 +0600

    added login.signup,dachboard path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router,  Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import "./App.css";
+import "../src/Styles/App.css";
 import Details from "./pages/Details";
 import Landing from "./pages/Landing";
 import Order from "./pages/Order";
 import OrderedList from "./Components/OrderedList";
+import Login from "./Auth/Login";
+import SignUp from "./Auth/SignUp";
+import Dashboard from "./Auth/DashBoard";
 
 
 function App() {
@@ -13,6 +16,9 @@ function App() {
     <>
       <Router>
         <Routes>
+        <Route exact path="/" element={<Login/>}/>
+        <Route exact path="/signup" element={<SignUp/>}/>
+        <Route exact path="/dashboard" element={<Dashboard/>}/>
         <Route exact path="/landing" element={<Landing/>}/>
         <Route exact path="/home" element={<Home/>}/>
         <Route exact path="/details" element={<Details/>}/>
