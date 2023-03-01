30006dfa978d3c97104b8bf0f5debcf361c106ba
mohammadismail <ismailalam901@gmail.com>
2023-03-01 17:20:37 +0600

    shows user name

--- src/Auth/DashBoard.js
+++ src/Auth/DashBoard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import firebase from "../Config/FireBase";
 import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
@@ -6,6 +6,23 @@ import { useNavigate } from "react-router-dom";
 const DashBoard = () => {
   const auth = getAuth();
   const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        // User is signed out
+        navigate("/");
+      }
+    });
+
+    // Cleanup function
+    return () => {
+      unsubscribe();
+    };
+  }, [auth, navigate]);
 
   let handleLogOut = () => {
     signOut(auth).then(() => {
@@ -14,30 +31,12 @@ const DashBoard = () => {
     });
   };
 
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      console.log(auth.currentUser);
-    } else {
-      // User is signed out
-      navigate("/");
-    }
-  });
-
   return (
-    <div className="home" style={{borderRadius:"10px", marginBottom:"20px"}}>
-      {auth.currentUser ? (
-        <>
-          <h2>{auth.currentUser.displayName}</h2>
-          <h2>{auth.currentUser.email}</h2>
-        </>
-      ) : (
-        <p>Loading...</p>
-      )}
-
+    <div className="home" style={{ borderRadius: "10px", marginBottom: "20px" }}>
+      <h2>Welcome, {user?.displayName}</h2>
       <button onClick={handleLogOut}>Log Out</button>
-      
     </div>
   );
-}  
+};
 
 export default DashBoard;
