70351eecc97d8260a5bb3b83bbb353b54834bb15
mohammadismail <ismailalam901@gmail.com>
2023-03-13 11:27:03 +0600

    Store userEmail

--- src/Auth/DashBoard.js
+++ src/Auth/DashBoard.js
@@ -1,26 +1,43 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import firebase from "../Config/FireBase";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import Landing from "../pages/Landing";
 import Header from "../Components/Header";
+import { userContext } from "../App";
 
 const DashBoard = () => {
   const auth = getAuth();
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
+  const [data, setData] = useState([]);
+
+  const context = useContext(userContext);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         setUser(user);
+        const email = localStorage.getItem('userEmail');
+        context.setUserEmail(email);
+
+        fetch(
+          "https://treat-management-system-691e2-default-rtdb.firebaseio.com/treats.json"
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            if (data) { // Check if data is null or undefined
+              const filteredData = Object.values(data).filter(
+                (treat) => treat.userEmail === email
+              );
+              setData(filteredData);
+            }
+          });
       } else {
-        // User is signed out
         navigate("/");
       }
-    },[user]);
+    });
 
-    // Cleanup function
     return () => {
       unsubscribe();
     };
@@ -29,9 +46,9 @@ const DashBoard = () => {
   return (
     <>
       <Header />
-      <div className="home" style={{ borderRadius: "10px", marginBottom: "20px" }}>
+      <div className="home" style={{ borderRadius: "10px", margin: "1em" }}>
         <h2>Welcome, {user?.displayName}</h2>
-        {user && <Landing />}
+        {user && <Landing data={data} />}
       </div>
     </>
   );
