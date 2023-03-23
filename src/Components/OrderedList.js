b256a67e0174b047d0183ae5ff564378f6c52137
mohammadismail <ismailalam901@gmail.com>
2023-03-23 11:19:25 +0600

    Gets all created treat form DB

--- src/Components/OrderedList.js
+++ src/Components/OrderedList.js
@@ -1,39 +1,159 @@
-import React, { useEffect, useState } from 'react';
+import { Button, Modal } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { MdDeleteForever } from "react-icons/md";
+import { BiCopy } from "react-icons/bi";
+import { TiTick } from "react-icons/ti";
+// import OrderedList from "../Menu/OrderedList";
 
-export default function OrderedList() {
-  const [list, setList] = useState([]);
+export default function Landing() {
+    const [show, setShow] = useState(false);
+    const [id, setId] = useState();
 
-  useEffect(() => {
-    fetch(`https://treat-management-system-691e2-default-rtdb.firebaseio.com/order.json`)
-      .then((response) => response.json())
-      .then((data) => {
-        setList(data);
-        console.log(data);
-      })
-      .catch((error) => {
-        console.log("Error fetching data: ", error);
-      });
-  }, []);
+    const handleClose = () => setShow(false);
 
-  return (
-    <div>
-        
-      <h2 className='orderList'>
-        OrderedList
-        </h2>
+    const handleShow = (id) => {
+        setShow(true);
+        setId(id);
+    };
 
-      {list && (
-        <ul className='border'>
-          {Object.values(list).map((item, index) => (
-            <li
-              key={index}
-              className="border"
+    const [data, setData] = useState([]);
+    const navigate = useNavigate();
+
+    //Get
+    useEffect(() => {
+        //     https://treat-management-system-691e2-default-rtdb.firebaseio.com/treats.json
+        fetch("https://treat-management-system-691e2-default-rtdb.firebaseio.com/treats.json")
+            .then((response) => response.json())
+            .then((data) => {
+                setData(data);
+            });
+    }, []);
+
+    //Delete
+    const handleDelete = (id) => {
+        fetch(`https://treat-management-system-691e2-default-rtdb.firebaseio.com/treats/${id}.json`, {
+            method: "DELETE",
+        })
+            .then(() => {
+                const newData = { ...data };
+                delete newData[id];
+                setData(newData);
+            });
+    };
+
+    function handleOnHome() {
+        navigate("/Home");
+    }
+
+    const [isCopied, setIsCopied] = useState(null);
+
+    const handleCopyClick = (id) => {
+        navigator.clipboard.writeText(id);
+        setIsCopied(id);
+
+        setTimeout(() => {
+            setIsCopied(null);
+        }, 3000);
+    };
+
+    return (
+        <>
+            <div>
+                {Object.entries(data).map(([key, value], index) => (
+        <li
+            key={index}
+            style={{
+                border: "1px solid grey",
+                borderRadius: "12px",
+                padding: "2em",
+                margin: "2em",
+                background: "aliceblue",
+                position: "relative",
+            }}
+        >
+            <button
+                type="button"
+                className="close"
+                aria-label="Delete"
+                variant="primary"
+                onClick={() => handleShow(key)}
+                style={{
+                    position: "absolute",
+                    top: "5px",
+                    right: "5em",
+                    borderRadius: "10px",
+                    border: "none",
+                }}
             >
-              <span>{`${item.itemsName} ---- ${item.name}`}</span>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
+                <MdDeleteForever />
+            </button>
+
+            <button
+                type="button"
+                className="copy"
+                aria-label="Copy"
+                variant="primary"
+                onClick={() => handleCopyClick(key)}
+                style={{
+                    position: "absolute",
+                    top: "5px",
+                    right: "1em",
+                    borderRadius: "10px",
+                    border: "none",
+                    height:"0"
+                }}
+            >
+                <BiCopy />
+                {isCopied === key && (
+                    <p className="success-message">
+                        <TiTick />
+                    </p>
+                )}
+            </button>
+
+            {/* <div style={{display: "flex"}}>
+                <span style={{fontWeight: "bold"}}>Created By:</span>
+                <span style={{marginLeft: "0.5em"}}>{value.name}</span>
+            </div> */}
+            
+            <div style={{display: "flex"}}>
+                <span style={{fontWeight: "bold"}}>Budget:</span>
+                <span style={{marginLeft: "0.5em"}}>{value.budgetLimitPerPerson}</span>
+            </div>
+            
+            {/* <div style={{display: "flex"}}>
+                <span style={{fontWeight: "bold"}}>Time:</span>
+                <span style={{marginLeft: "0.5em"}}>{value.timeLimit}</span>
+            </div> */}
+
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Price</th>
+                                    <th>Orderd By</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+
+                                {value.manualMenuList?.map((item, index) => {
+                                    // console.log(item);
+                                    return (
+                                        <tr key={index}>
+                                            <td>{item?.name}</td>
+                                            <td>{item?.price}</td>
+                                            <td>{item?.selectedBy?.map((person) => person.name).join(", ")}</td>
+                                        </tr>
+                                    );
+                                })}
+
+                            </tbody>
+                        </table>
+                    </li>
+                ))}
+            </div>
+        </>
+    );
 }
