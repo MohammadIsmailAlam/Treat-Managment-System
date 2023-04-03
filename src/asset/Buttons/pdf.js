de8d1e304ddfcbad9aed95eaf3663066752a6e66
mohammadismail <ismailalam901@gmail.com>
2023-04-03 12:07:10 +0600

    pdf component created,

---
+++ src/asset/Buttons/pdf.js
@@ -0,0 +1,35 @@
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+
+export const handlePDFDownload = (id, data) => {
+  // Create a new jsPDF instance
+  const doc = new jsPDF();
+
+  // Add content to the document
+  Object.entries(data).forEach(([key, value]) => {
+    if (key === id) {
+      doc.text(`Treat Caption: ${value.caption}`, 10, 10);
+
+      const tableData = [];
+      tableData.push(["Name", "Price", "Ordered By", "Total"]);
+
+      value.manualMenuList.forEach((item) => {
+        const selectedBy = item.selectedBy
+          .map((person) => person.name)
+          .join(", ");
+        const selectedCount = item.selectedBy.length;
+
+        tableData.push([item.name, item.price, selectedBy, selectedCount]);
+      });
+
+      doc.autoTable({
+        head: tableData.slice(0, 1),
+        body: tableData.slice(1),
+        startY: 20,
+      });
+    }
+  });
+
+  // Download the PDF document
+  doc.save(`${data[id].caption}.pdf`);
+};
